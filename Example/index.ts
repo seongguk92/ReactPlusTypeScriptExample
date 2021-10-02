bec6398c6ce04b1ff8a235d0961d6df0dbf148b3
seongguk <live8681@naver.com>
2021-10-02 16:47:36 +0900

    #8.1 Introduction to Typescript

---
+++ Example/index.ts
@@ -0,0 +1,10 @@
+
+
+//#8.1 Introduction to Typescript
+//before
+// const puls = (a, b) => a + b;
+// console.log(puls(1, 2));
+
+
+const puls = (a:number, b:number) => a + b;
+console.log(puls(1, 2));

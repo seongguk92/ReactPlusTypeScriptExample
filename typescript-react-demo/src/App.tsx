8ceec8a0baad0e9e7cc7bc2c1a2415a36c9de405
seongguk <live8681@naver.com>
2021-10-02 23:33:07 +0900

    #8.4 React State and Typescript

---
+++ typescript-react-demo/src/App.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+interface IState {
+  counter: number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0
+  };
+  
+  render() {
+    const { counter } = this.state;
+    return <div>{counter} <button onClick={this.add}>Add</button></div>;
+  }
+  add = ():void => {
+    this.setState(prev => {
+      return {
+        counter: prev.counter + 1
+      }
+    })
+  }
+}
+
+export default App;

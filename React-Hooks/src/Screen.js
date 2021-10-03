ab34c72502263153047e6b4e75f869cb649b4c4f
seongguk <live8681@naver.com>
2021-10-03 14:43:07 +0900

    #9.2 useContext in Action

---
+++ React-Hooks/src/Screen.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import Header from "./Header";
+import { UserContext } from "./context";
+
+const Screen = () => {
+    const { logUserIn } = useContext(UserContext);
+    return (
+        <div>
+            <Header />
+            <h1>First scren</h1>
+            <button onClick={logUserIn}>Log user in</button>
+        </div>
+    );
+};
+
+export default Screen;
